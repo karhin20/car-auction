826e9bcd9c878e5f5626ab1c52b91ad31bf41841
karhin20 <kelvinarhin25@gmail.com>
2024-05-27 01:02:17 +0000

    hello res

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,18 @@ const UserInfoModel = require('./schema/userInfoModel.js');
 const BookingModel = require('./schema/bookingModel.js');
 
 
-app.use(cors())
+app.use(cors(
+  {
+    origin: ["front-end-auction" , "localhost:3000"],
+    methods: ["POST" , "GET", "DELETE"],
+    credentials: true
+  }
+))
+
+app.get("/", (req , res) => {
+  res.json("Hello")
+})
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use((req, res, next) => {
